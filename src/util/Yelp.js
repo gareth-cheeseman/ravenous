c152e05192f01e0a261e47a3984d2addba777868
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-05-29 15:16:32 +0100

    links up to api

---
+++ src/util/Yelp.js
@@ -0,0 +1,28 @@
+const apiKey =
+  'b8Oxu5EHvqvVcFgVFIcBQhOP3zXPfvnkVIJc-VAqe8Vv6KWgDnGtri15FLhM_asn1sy0xYcjsmqNUmx6CjuwIfsNlEgNlGCgJI-HryUY6GhroktkxHRyeMnfOCoNW3Yx';
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      { headers: { Authorization: `Bearer ${apiKey}` } }
+    )
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }));
+        }
+      });
+  }
+};
+export default Yelp;
